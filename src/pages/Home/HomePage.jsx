ffe0f86a3723bdd2292c1bdd2e5f8ae7b1bc05bf
OhMyLaia <ohmylaia@live.com>
2025-11-07 15:27:08 +0100

    created searchbar's UI, logics and state to handle query, WIP: onkeydown, clean input

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -8,9 +8,30 @@ import { useState, useEffect } from 'react';
 function HomePage() {
 
     const [filteredSessions, setFilteredSessions] = useState(SESSIONS);
+    const [query, setQuery] = useState("");
+
+    const handleSearch = async (e) => {
+        const newQuery = e.target.value;
+        setQuery(newQuery);
+        const sessionsByQuery = await searchSessions(newQuery);
+        setFilteredSessions(sessionsByQuery);
+    }
+
+    useEffect(() => {
+        return () => {
+            setQuery("");
+        };
+    }, []);
 
     return (
         <main className='m-5'>
+            <div className='ms-5 flex flex-row w-full justify-start items-center gap-x-2'>
+                <label></label>
+                <BiSearch/>
+                <input value={query}
+                onChange={handleSearch}
+                className='p-1 w-[80%] md:w-1/2 border-violet-400 rounded-xl bg-violet-100' placeholder='Search by title, track or speaker...'></input>
+            </div>
             <CardList>
                 {filteredSessions.map(session => (
                     <Card
