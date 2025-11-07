963805cf0e51d736908c436db1e32339aa5b83c0
OhMyLaia <ohmylaia@live.com>
2025-11-07 15:08:15 +0100

    created logics and used CardList and Card  to show only scheduled sessions

--- src/pages/MySchedule/MySchedulePage.jsx
+++ src/pages/MySchedule/MySchedulePage.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { SESSIONS } from "../../api";
+import { useSchedule } from "../../context/ScheduleContext";
+import Card from "../../components/Card";
+import CardList from "../../components/CardList";
+
 
 function MySchedulePage() {
+
+    const { sessionIds } = useSchedule();
+
+    // we keep the ones included in sessionIds array too
+    // so we itereate real time data
+    const scheduledSessions = SESSIONS.filter(session =>
+        sessionIds.includes(session.id)
+    );
+
     return (
-        <div>MySchedulePage</div>
+        <main className='m-5'>
+            <h2 className='text-3xl'>My scheduled sessions</h2>
+            <p className='text-xl'>You have {sessionIds.length} upcoming {`${sessionIds.length === 1 ? "session" : "sessions"} `}</p>
+            <CardList>
+                {scheduledSessions.map(session => (
+                    <Card
+                        key={session.id}
+                        session={session}
+                    />
+                ))}
+            </CardList>
+        </main>
     )
 }
 
