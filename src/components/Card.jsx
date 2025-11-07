7cb5ab8c8e7bcd06d29e770e57b882030e29eb8b
OhMyLaia <ohmylaia@live.com>
2025-11-07 16:17:58 +0100

    styled cards, changed track colour depending on its value

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -21,9 +21,12 @@ function Card({ session }) {
 
 
     return (
-        <div>
-            <div>
-                <div>
+        <div className='flex flex-row h-52 gap-3 bg-white rounded-br-4xl rounded-xl p-3
+        justify-between shadow-lg hover:scale-102 transition-transform 
+        duration-300 
+        ease-in-out'>
+            <div className=' flex flex-col w-2/3'>
+                <div className="grow">
                     <h3 className="font-bold">{session.title ? session.title : ''}</h3>
                     <p>{session.speaker}</p>
                     <p>Starts at: {new Date(session.startsAt).toLocaleTimeString([], {
@@ -31,21 +34,23 @@ function Card({ session }) {
                         minute: '2-digit',
                         hour12: false // avoids am/pm stuff
                     })}h</p>
-                    <div>
+                    <div className="flex flex-row items-center gap-x-2">
                         <BsClock></BsClock>
                         <p>{session.durationMins} min</p>
                     </div>
                 </div>
-                <div>
+                <div className="flex flex-col items-start">
                     <PrimaryButton
                         onClick={() => toggleSchedule(session)}
                         children={isAttending ? "Remove" : "Add"}
+                        className={isAttending ? "bg-red-500 hover:bg-red-700 text-white" : "bg-violet-500 hover:bg-violet-700 text-white"}
                     />
                 </div>
             </div>
-            <aside>
-                <p>{session.track}</p>
+            <aside className='justify-end items-end text-center w-fit'>
+                <p className={`${getTrackColour(session.track)} text-black rounded-full py-2 px-4`}>{session.track}</p>
             </aside>
+
         </div>
     )
 }
